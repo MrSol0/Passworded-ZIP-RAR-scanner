e887a2451ef322074a20935bfb44eea5ed72c191
MrSol0 <35892335+MrSol0@users.noreply.github.com>
2021-02-28 21:57:09 +0100

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,49 @@
+import zipfile
+import rarfile
+from os import walk
+
+class client(object):
+    def __init__(self,path):
+        self.encrypted = {}
+        self.find_all(path)
+    def find_all(self, path):
+        _, _, filenames = next(walk(path))
+        for file in filenames:
+            if file.endswith(".zip"):
+                self.checkZIPEncryption(path+"\\"+file,file)
+            elif file.endswith(".rar"):
+                self.checkRAREncryption(path+"\\"+file,file)
+
+        for file in self.encrypted:
+            print('%s is encrypted!' % file)
+
+    def checkZIPEncryption(self,path,file):
+        if self.encrypted.get(file):
+            return
+        try:
+            zf = zipfile.ZipFile(path)
+        except:
+            return
+
+        for zinfo in zf.infolist():
+            is_encrypted = zinfo.flag_bits & 0x1
+            if is_encrypted:
+                #print ('%s is encrypted!' % path)
+                self.encrypted[file] = True
+    def checkRAREncryption(self,path,file):
+        if self.encrypted.get(file):
+            return
+        try:
+            rar = rarfile.RarFile(path)
+        except:
+            return
+        if rar.needs_password():
+            self.encrypted[file] = True
+
+
+
+
+if __name__ == "__main__":
+    print("[SYNTAX]: C:\Program Files (x86)\Adobe\Adobe Utilities - CS5\ExtendScript Toolkit CS5\Legal")
+    path = input("[FOLDER PATH]: ")
+    client(path)
